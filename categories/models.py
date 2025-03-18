7027b995a2d0f0b5fdc1e74fc6e494171d327542
Juan Pino <pinjup@protonmail.com>
2025-03-18 18:57:55 -0500

    Create the modeling, serialization, and view of the 'categories' app

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="categories", blank=True, null=True)
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+)
+
+from categories.models import Category
+from categories.serializers import CategorySerializer
 
 # Create your views here.
+class ListCategoryView(ListAPIView, CreateAPIView):
+    allowed_methods = ['GET', 'POST']
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
