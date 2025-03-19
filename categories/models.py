80b7bd565a1c5888f47f7ebe005e4e9f23738fad
Juan Pino <pinjup@protonmail.com>
2025-03-18 19:02:36 -0500

    I made a mistake and added a field that I shouldn't add at the moment.

--- categories/models.py
+++ categories/models.py
@@ -5,4 +5,3 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="categories", blank=True, null=True)
