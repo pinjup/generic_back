bd1b9321743e23a0243543046d994cc7f6041a76
Juan Pino <pinjup@protonmail.com>
2025-03-20 05:13:36 -0500

    Add comments to see the endpoints

--- categories/views.py
+++ categories/views.py
@@ -8,12 +8,16 @@ from rest_framework.generics import (
 from categories.models import Category
 from categories.serializers import CategorySerializer
 
-# Create your views here.
+# GET api/categories/
+# POST api/categories/
 class ListCategoryView(ListAPIView, CreateAPIView):
     allowed_methods = ['GET', 'POST']
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
+# GET api/categories/<id>/
+# PUT api/categories/<id>/
+# DELETE api/categories/<id>/
 class DetailCategoryView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
     serializer_class = CategorySerializer
--- products/views.py
+++ products/views.py
@@ -7,12 +7,16 @@ from rest_framework.generics import (
 from products.models import Product
 from products.serializers import ProductSerializer
 
-# Create your views here.
+# GET api/products/
+# POST api/products/
 class ListProductView(ListAPIView, CreateAPIView):
     allowed_methods = ['GET', 'POST']
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
+# GET api/products/<id>/
+# PUT api/products/<id>/
+# DELETE api/products/<id>/
 class DetailProductView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
     serializer_class = ProductSerializer
