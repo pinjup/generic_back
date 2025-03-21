b0cc76226d9ca85ef62761e39d30e00dd0d4790f
Juan Pino <pinjup@protonmail.com>
2025-03-20 19:00:52 -0500

    Add Login Check

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import UserListView, UserRegistrationView
+from accounts.views import LoginAPIView, UserListView, UserRegistrationView
 
 urlpatterns = [
     path('accounts/register/', UserRegistrationView.as_view(), name='user-registration'),
     path("users/", UserListView.as_view(), name="user-list"),
+    path("login/", LoginAPIView.as_view(), name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,42 @@
-from rest_framework.generics import CreateAPIView,ListAPIView
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from accounts.serializers import UserRegistrationSerializer, UserListSerializer
 
 User = get_user_model()
+
+
 class UserRegistrationView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
+
 
 class UserListView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
+
+
+class LoginAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"detail": "Both username and password are required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response(
+                {"detail": "Credentials are valid."}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"detail": "Invalid username or password."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
