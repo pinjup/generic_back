6027f942a4c25802994b0cb628d29bd4ef55a8ef
Juan Pino <pinjup@proton.me>
2025-04-07 10:40:22 -0500

    Remove token creation automatically

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,10 +4,6 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from accounts.models import UserProfile
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance=None, created=False, **kwargs):
